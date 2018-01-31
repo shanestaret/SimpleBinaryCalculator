6e17eacd37a1159e57a55dfa5d5862aebe86fd86
IanSaucy <30986157+IanSaucy@users.noreply.github.com>
2018-01-31 15:04:12 -0500

    Updated register with add/setter fucntions

--- src/TwosComplementer.java
+++ src/TwosComplementer.java
@@ -1,7 +1,43 @@
 public class TwosComplementer {
 
     public static Register twosComplement(Register register) {
-        return new Register(0);
+
+        //Get register value
+        boolean[] registerVal = register.getRegisterValue();
+        //Get complement
+        registerVal = getComplement(registerVal);
+        //Add one
+
+
+
+    }
+
+    private static boolean[] getComplement(boolean[] complement) {
+        for (int i = 0; i < complement.length; i++) {
+            LogicGate notGate = new LogicGate(LogicGate.NOT);
+            notGate.setInput1(complement[i]);
+            complement[i] = notGate.getOutput();
+        }
+        return complement;
+
+    }
+
+    private static boolean[] addOne(boolean[] addend){
+        //Add one to value
+        LogicGate xorGate = new LogicGate(LogicGate.XOR);
+        LogicGate andGate = new LogicGate(LogicGate.AND);
+        for (int i = 0; i < addend.length; i++) {
+            //Our "1" to add
+            boolean a = true;
+            xorGate.setInput1(a);
+            andGate.setInput1(a);
+            xorGate.setInput2(addend[i]);
+            andGate.setInput2(addend[i]);
+
+            //Set new values
+            addend[i] = xorGate.getOutput();
+        }
+
     }
 
 
