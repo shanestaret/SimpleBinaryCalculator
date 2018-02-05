ac84e4241f9da9a48629aa68307f7f5796570ac6
IanSaucy <30986157+IanSaucy@users.noreply.github.com>
2018-02-05 15:34:33 -0500

    Testing progam not working,

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,45 +1,33 @@
 public class Calculator {
     public static void main(String[] args) {
- /*       Register A = new Register(8);
+        Register A = new Register(8);
         Register B = new Register(8);
-        A.clockIt();
-        A.setDatalines("00110010");
-        A.clockIt();
-        B.clockIt();
-        B.setDatalines("00001101");
-        B.clockIt();
-        System.out.printf(" %s (%3d)%n", A, A.toDecimalNumber());
-        System.out.printf("+%s (%3d)%n", B, B.toDecimalNumber());
+        boolean[] regAVal = new boolean[]{false, false, true, true, false, false, true, false};
+        boolean[] regBVal = new boolean[]{false, false, false, false, true, true, false, true};
+        A.setRegisterValue(regAVal);
+        B.setRegisterValue(regBVal);
+
+        System.out.printf(" %s %s", A, A);
+        System.out.printf("+%s %s", B, B);
         System.out.printf("---------------------------------%n");
         Register R = Adder.add(A, B);
-        System.out.printf(" %s (%3d)%n", R, R.toDecimalNumber());
+        System.out.printf(" %s (%3d)%n", R, R);
         System.out.printf("%n");
         System.out.printf("If we had hardware to subtract, here is what it would look like%n");
-        System.out.printf(" %s (%3d)%n", A, A.toDecimalNumber());
-        System.out.printf("-%s (%3d)%n", B, B.toDecimalNumber());
+        System.out.printf(" %s (%3d)%n", A, A);
+        System.out.printf("-%s (%3d)%n", B, B);
         System.out.printf("---------------------------------%n");
         // adding the twos complement is how we subtract
         Register C = TwosComplementer.twosComplement(B);
         R = Adder.add(A, C);
-        System.out.printf("" + " %s (%3d)%n", R, R.toDecimalNumber());
+        System.out.printf("" + " %s (%3d)%n", R, R);
         System.out.printf("%n");
         System.out.printf("But we don't so we add the twos complement and here is what that would look like%n");
-        System.out.printf(" %s (%3d)%n", A, A.toDecimalNumber());
-        System.out.printf("+%s (%3d)%n", C, C.toDecimalNumber());
+        System.out.printf(" %s (%3d)%n", A, A);
+        System.out.printf("+%s (%3d)%n", C, C);
         System.out.printf("---------------------------------%n");
         // adding the twos complement is how we subtract
-        System.out.printf("" + " %s (%3d)%n", R, R.toDecimalNumber());*/
-
-        boolean[] register = {true, true, false, true};
-
-        TwosComplementer complementer = new TwosComplementer();
-        register = complementer.addOne(register);
-        register = complementer.get
-
-        for (int i = 0; i < register.length; i++) {
-            System.out.println(register[i]);
-
-        }
+        System.out.printf("" + " %s (%3d)%n", R, R);
 
     }
 }
--- src/Register.java
+++ src/Register.java
@@ -1,14 +1,38 @@
 
 public class Register {
+    boolean[] register;
 
-	boolean[] Register = new boolean[8];	
-	
+    public Register(int size){
+        register = new boolean[size];
+
+    }
+
+    public Register(){
+        register = new boolean[8];
+
+    }
+
+
 	public boolean[] getRegisterValue() {
-		return Register;
+		return register;
 	}
 	
 	public void setRegisterValue(boolean[] value) {
 		for (int i = 1; i < 8; i++)
-			Register[i] = value[i];
-	}	
+			register[i] = value[i];
+	}
+
+	public String toString(){
+        String tempString = "";
+        for (int i = 0; i < register.length; i++) {
+            if(register[i]){
+                tempString += "1";
+            }
+            else{
+                tempString += "0";
+            }
+
+        }
+        return tempString;
+    }
 }
